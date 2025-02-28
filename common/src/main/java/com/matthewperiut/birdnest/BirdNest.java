2e1678036eefc6f9e1e8b4aa20271f1cc414d48d
Matthew Periut <matthewperiut@gmail.com>
2025-02-28 11:23:31 -0500

    Config library added

--- common/src/main/java/com/matthewperiut/birdnest/BirdNest.java
+++ common/src/main/java/com/matthewperiut/birdnest/BirdNest.java
@@ -1,6 +1,7 @@
 package com.matthewperiut.birdnest;
 
-import com.matthewperiut.birdnest.config.BirdNestConfig;
+import com.periut.cryonicconfig.ConfigStorage;
+import com.periut.cryonicconfig.CryonicConfig;
 import com.matthewperiut.birdnest.event.BirdNestEvents;
 import com.matthewperiut.birdnest.item.BirdNestItems;
 import org.slf4j.Logger;
@@ -10,10 +11,10 @@ public class BirdNest
 {
 	public static final String MOD_ID = "birdnest";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static ConfigStorage CONFIG = CryonicConfig.getConfig(MOD_ID);
 
 	public static void init()
 	{
-		BirdNestConfig.init();
 		BirdNestItems.initialize();
 		BirdNestEvents.initialize();
 	}
